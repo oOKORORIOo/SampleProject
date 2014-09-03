4d0e8cdc1d7c43b23de2a5b8a28cfbfb3546ed1d
KazumaMori <kazumathegreat@gmail.com>
2014-09-03 11:44:54 +0900

    三宇ぷる

---
+++ sample.cpp
@@ -0,0 +1,22 @@
+/**
+* @file		CLASS.h
+* @brief
+* @date
+* @author	Kazuma Mori
+*/
+
+// インクルードガード
+#ifndef _INC_CLASS_H
+#define _INC_CLASS_H
+
+/**
+*	CLASSクラス
+*/
+class CLASS
+{
+public:
+	CLASS(void);
+	virtual ~CLASS(void);
+};
+
+#endif
